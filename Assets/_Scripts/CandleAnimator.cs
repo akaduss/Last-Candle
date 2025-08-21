1e6cda6e7213c3e40ab267c19c0c35cc78f555b1
akaduss <61531800+akaduss@users.noreply.github.com>
2025-08-21 17:13:22 +0300

    Early

--- Assets/_Scripts/CandleAnimator.cs
+++ Assets/_Scripts/CandleAnimator.cs
@@ -1,30 +1,69 @@
+using System;
 using UnityEngine;
 
 public class CandleAnimator : MonoBehaviour
 {
     public ParticleSystem fireParticle;
+    public Light pointLight;
+    public float lightIntensity = 1f;
+    public float lightRange = 1f;
     public float burnTime;
     private float curTime;
 
     private void Start()
     {
-
+        pointLight.intensity = 0;
         curTime = burnTime;
     }
+
+    void OnEnable()
+    {
+        Game.OnCandleLit += Game_OnCandleLit;
+    }
+
+    void OnDisable()
+    {
+        Game.OnCandleLit -= Game_OnCandleLit;
+    }
+
+    private void Game_OnCandleLit(float flameTime)
+    {
+        fireParticle.Play();
+        curTime = flameTime;
+    }
+
 
     private void Update()
     {
         if (!fireParticle.isPlaying)
             return;
-        
-        if (curTime < 0)
-            curTime = burnTime;
+
+
         curTime -= Time.deltaTime;
 
         var mainMod = fireParticle.main;
         mainMod.startSize = Mathf.Lerp(0.3f, 1, curTime / burnTime);
         mainMod.startLifetime = Mathf.Lerp(0.3f, 1, curTime / burnTime);
-        //print(Mathf.Lerp(0.3f, 1, curTime / burnTime) +"::" + curTime / burnTime);
+
+        //if normalized time < 0.5 lower range of light to %50 of original and intensity
+        if (curTime / burnTime < 0.5f)
+        {
+            pointLight.intensity = Mathf.Lerp(lightIntensity * 0.5f, lightIntensity, curTime / (burnTime * 0.5f));
+            pointLight.range = Mathf.Lerp(lightRange * 0.5f, lightRange, curTime / (burnTime * 0.5f));
+        }
+        else if (curTime / burnTime >= 0.5f)
+        {
+            pointLight.intensity = lightIntensity;
+            pointLight.range = lightRange;
+        }
+
+        if (curTime <= 0)
+        {
+            fireParticle.Stop();
+            pointLight.intensity = 0;
+            pointLight.range = 0;
+        }
+        print("CandleAnimator Update: " + curTime);
     }
 
 
--- Assets/_Scripts/Game.cs
+++ Assets/_Scripts/Game.cs
@@ -1,26 +1,83 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+using System;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
+    public static event Action<float> OnCandleLit;
+
+    [Header("Candle Settings")]
+    public float flameTime;
+    private float flameTimeLeft;
+
+    [Header("UI")]
+    public TextMeshProUGUI lightText;
+    public Slider flameSlider;
+
     public float Light;
     public bool isLit;
     InputAction playAct;
     CandleAnimator candleAnimator;
+    PlayerInput playerInput;
 
     void Start()
     {
+        // Keep Start lean; initialization is handled in Awake/OnEnable to be
+        // robust when Domain Reload is disabled.
+        Light = 0;
+        flameSlider.value = 0;
+        flameSlider.maxValue = flameTime;
+    }
+
+    void Awake()
+    {
         isLit = false;
         candleAnimator = GetComponent<CandleAnimator>();
-        playAct = InputSystem.actions.FindAction("Interact");
-        playAct.performed += PlayAct_performed; ;
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    void OnEnable()
+    {
+        // Prefer the PlayerInput's action asset if present, otherwise fall back
+        // to the global InputSystem.actions. This is safe when domain reload
+        // is disabled because we resolve actions at enable time.
+        var asset = playerInput != null ? playerInput.actions : InputSystem.actions;
+        playAct = asset != null ? asset.FindAction("Interact") : null;
+
+        if (playAct != null)
+        {
+            playAct.performed += PlayAct_performed;
+            if (!playAct.enabled)
+                playAct.Enable();
+        }
+        else
+        {
+            Debug.LogWarning("Interact action not found on InputActionAsset.");
+        }
+    }
+
+    void OnDisable()
+    {
+        if (playAct != null)
+        {
+            playAct.performed -= PlayAct_performed;
+            if (playAct.enabled)
+                playAct.Disable();
+            playAct = null;
+        }
     }
 
     private void PlayAct_performed(InputAction.CallbackContext obj)
     {
+        print("Candle lit");
         if (isLit)
             return;
 
+        OnCandleLit?.Invoke(flameTime);
+        flameTimeLeft = flameTime;
+        candleAnimator.burnTime = flameTime;
         candleAnimator.fireParticle.Play();
         isLit = true;
     }
@@ -29,7 +86,18 @@ public class Game : MonoBehaviour
     {
         if (isLit)
         {
+            flameTimeLeft -= Time.deltaTime;
             IncreaseLight();
+
+            lightText.text = "Light: " + Light.ToString("F1");
+            flameSlider.value = flameTimeLeft;
+
+            if (flameTimeLeft <= 0)
+            {
+                isLit = false;
+                candleAnimator.fireParticle.Stop();
+                flameTimeLeft = flameTime;
+            }
         }
     }
 
@@ -37,5 +105,13 @@ public class Game : MonoBehaviour
     {
         Light++;
         print(Light);
+    }
+
+    void OnDestroy()
+    {
+        // OnDisable already unsubscribes; keep OnDestroy safe in case it is
+        // called without OnDisable.
+        if (playAct != null)
+            playAct.performed -= PlayAct_performed;
     }
 }
